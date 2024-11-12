78debc918e74859128418c74a791fb1cd645d8ea
yerrouhamza <hamzayerrou5@yahoo.com>
2024-11-12 16:06:52 +0100

    created toggle button and make it pass the value to the parent

---
+++ src/components/Ui-elements/ToggleButtons.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+    import { PropType } from 'vue';
+
+    const props = defineProps({
+        value: {
+            type: String,
+            required: true,
+        },
+        options: {
+            type: Array as PropType<string[]>,
+            required: true,
+        },
+        setTheOption: {
+            type: Function as PropType<(option: string) => void>,
+            required: true,
+        },
+    });
+    
+    const toggleOption = (option: string) => {
+        props.setTheOption(option);
+    };
+</script>
+
+<template>
+  <div class="flex items-center p-1 bg-gray-200 rounded-lg w-full">
+    <button
+        :class="[
+            'text-gray-500 px-4 py-1 rounded-lg transition-all duration-200 flex-1',
+            value === option && 'bg-white text-gray-800 shadow'
+        ]"
+        v-for="option in options"
+        @click="toggleOption(option)"
+    >
+        {{ option }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+</style>
