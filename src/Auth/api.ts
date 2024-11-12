182a717e22decc7534924785c63f06e5093baf59
yerrouhamza <hamzayerrou5@yahoo.com>
2024-11-12 11:55:13 +0100

    chnage font to Inter and added gradient-dark bg

--- src/Auth/api.ts
+++ src/Auth/api.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://api.weatherapi.com/v1',
+  baseURL: 'http://api.weatherapi.com/v1/',
   timeout: 10000, // 10 seconds
   headers: {
     'Content-Type': 'application/json',
   },
   params: {
-    key: 'e2b1c7a4a7e74e6a8b1133733210804',
+    key: '8ad0d366eba14f8087e220940241111',
   },
 });
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-dark': 'linear-gradient(4deg, rgba(16,20,29,1) 0%, rgba(31,36,44,1) 39%, rgba(45,51,62,1) 72%)',
+      },
+    },
   },
   plugins: [],
 }
