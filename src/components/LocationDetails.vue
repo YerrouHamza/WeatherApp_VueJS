8581625339db5940a5993e40462374bd177096a7
yerrouhamza <hamzayerrou5@yahoo.com>
2024-11-12 19:06:59 +0100

    created Component for show current location with date

---
+++ src/components/LocationDetails.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+    import { ref, defineProps, onMounted } from 'vue';
+    import moment from 'moment';
+
+    const currentTime = ref(moment())
+    
+    const props = defineProps({
+        location: {
+            type: Object,
+            required: true,
+        },
+        currentTime: {
+            type: String,
+            required: true,
+        },
+    });
+  
+    onMounted(async() => {
+        setInterval(() => {
+            currentTime.value = moment()
+        }, 10000)
+    })
+</script>
+
+<template>
+    <div class="space-y-1">
+        <h1 class="text-5xl font-semibold">{{ props.location?.name }}</h1>
+        <p class="text-gray-600">{{ moment(props.currentTime).format('dddd, MMMM D, YYYY') }}</p>
+        <p class="text-gray-600">{{ moment(props.currentTime).format('HH:mm A') }}</p>
+    </div>
+</template>
+
+<style scoped>
+</style>
